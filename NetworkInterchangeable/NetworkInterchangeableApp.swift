407e1e5360630b1fdf90b1e1aa2228b5f161a90e
yigit <yigitozdamar@hotmail.com>
2022-08-27 19:37:32 +0300

    Initial Commit

---
+++ NetworkInterchangeable/NetworkInterchangeableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangeableApp.swift
+//  NetworkInterchangeable
+//
+//  Created by Yigit Ozdamar on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangeableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
